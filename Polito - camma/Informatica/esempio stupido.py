afcec8e69fd20258788e7c72eb247a48a45d366b
alexein1001 <alessiocammarata42@gmail.com>
2024-10-16 15:35:09 +0200

    Lezione7

--- Polito - camma/Informatica/esempio stupido.py
+++ Polito - camma/Informatica/esempio stupido.py
@@ -1,7 +1,11 @@
-versamento = 10000
-i = 0
-while versamento < 20000:
-	i+=1
-	versamento += (versamento * 5)/100
-	
-print(i)
+altezza = int(input("Inserisci l'altezza dell'albero di natale:\n"))
+# while altezza > 10 or altezza <= 0 or not altezza.isdigit():
+while altezza > 10 or altezza <= 0:
+	print("Bestia")
+	altezza = int(input("Reinserisci l'altezza dell'albero di natale:\n"))
+
+h = altezza * 2 + 1
+for i in range(1, h, 2):
+	s = i * "*"
+	print(f"{s:^{h-1}}")
+
