a002e97bf0d2a1e517fae2e14cfc0f78f078b216
Alexein <alessiocammarata42@gmail.com>
2024-11-19 18:17:37 +0100

    clear

--- Polito - camma/Informatica/esempi_lezione2.py
+++
@@ -1,9 +0,0 @@
-lista = [3, 1, 2]
-
-lista2 = lista[:] #crea una copia della lista
-
-lista[0:1] = [5, 6] 
-print(lista) # -> [5, 6, 2]
-
-lista[0:1] = [] #posso anche restringere una lista in questo modo
-print(lista) # -> [2]
--- Polito - camma/Informatica/read_file.py
+++
@@ -1,8 +0,0 @@
-FILENAME = "story.txt"
-file = open(FILENAME,"r")
-
-voti = [float(row) for row in file ]
-
-if len(voti) != 0:
-    print("La media è ", sum(voti)/len(voti))
-file.close()
